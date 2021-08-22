52597924ebc67c8a47942ee6b3c59aab95e99765
majdhasan <majd.hasan@hotmail.com>
2021-08-22 16:14:29 +0200

    add runner test

--- src/main/java/life/majd/stonies/StoniesApplication.java
+++ src/main/java/life/majd/stonies/StoniesApplication.java
@@ -1,13 +1,32 @@
 package life.majd.stonies;
 
+import com.mongodb.client.model.geojson.Point;
+import com.mongodb.client.model.geojson.Position;
+import life.majd.stonies.domain.Stone;
+import life.majd.stonies.service.StoneService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static java.time.LocalDateTime.now;
 
 @SpringBootApplication
 public class StoniesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StoniesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StoniesApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(StoneService stoneService) {
+        return args -> {
+            Stone stone = new Stone(null, "title", "content", 12.44444,12.234234, 10, null, LocalDate.now(), now(), "author", "imageUrl", null);
+            stoneService.addStone(stone);
+        };
+    }
 
 }
