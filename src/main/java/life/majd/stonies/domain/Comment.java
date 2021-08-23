9562ed19a1f28f2718c54b393c06f7718bb60f27
majdhasan <majd.hasan@hotmail.com>
2021-08-23 12:55:23 +0200

    add lombok annotations

--- src/main/java/life/majd/stonies/domain/Comment.java
+++ src/main/java/life/majd/stonies/domain/Comment.java
@@ -1,7 +1,14 @@
 package life.majd.stonies.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
 
     private String authorId;
