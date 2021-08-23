23255556d9f246b22e542bf276aa6a62b8ea5735
majdhasan <majd.hasan@hotmail.com>
2021-08-23 12:56:11 +0200

    add comment and voting rest endpoints

--- src/main/java/life/majd/stonies/controller/StoneController.java
+++ src/main/java/life/majd/stonies/controller/StoneController.java
@@ -1,5 +1,6 @@
 package life.majd.stonies.controller;
 
+import life.majd.stonies.domain.Comment;
 import life.majd.stonies.domain.Stone;
 import life.majd.stonies.service.StoneService;
 import lombok.RequiredArgsConstructor;
@@ -29,8 +30,27 @@ public class StoneController {
         return stoneService.getStone(id);
     }
 
+    @PostMapping("/{id}")
+    public void addComment(@PathVariable String id, @RequestBody Comment comment) {
+        stoneService.addComment(id, comment);
+    }
+
     @DeleteMapping("/{id}")
     public void deleteStoneById(@PathVariable String id) {
         stoneService.deleteStone(id);
     }
+
+    @PostMapping("/{stoneId}/upvote")
+    public void upVoteStone(@PathVariable String stoneId) {
+        //TODO add username fetching from jwt
+        stoneService.upVoteStoneById(stoneId, "userId");
+    }
+
+    @PostMapping("/{stoneId}/downvote")
+    public void downVoteStone(@PathVariable String stoneId) {
+        //TODO add username fetching from jwt
+        stoneService.downVoteStoneById(stoneId, "userId");
+    }
+
+
 }
